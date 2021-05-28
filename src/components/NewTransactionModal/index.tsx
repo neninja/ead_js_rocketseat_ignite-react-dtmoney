3e27d285e515abe06f8bf075f25e2716d34020d6
Felipe Silva <felipe.dacs@gmail.com>
2021-05-27 22:27:35 -0300

    :tv: add aula 13

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from 'react-modal'
 
+import { Container } from './styles'
+
 Modal.setAppElement('#root')
 
 interface NewTransactionModalProps {
@@ -12,8 +14,29 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       <Modal
         isOpen={isOpen}
         onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
       >
-        <h2>Cadastrar Transação</h2>
+        <Container>
+          <h2>Cadastrar Transação</h2>
+
+          <input 
+            placeholder="Título"
+          />
+
+          <input 
+            type="number"
+            placeholder="Valor"
+          />
+
+          <input 
+            placeholder="Categoria"
+          />
+
+          <button type="submit">
+            Cadastrar
+          </button>
+        </Container>
       </Modal>
   )
 }
