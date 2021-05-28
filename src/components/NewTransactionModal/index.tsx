056f37b32db233a6fcafd057142f0d551c1edec6
Felipe Silva <felipe.dacs@gmail.com>
2021-05-27 23:46:26 -0300

    :tv: add aula 16

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
+import { useState } from 'react'
 import Modal from 'react-modal'
 
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 
-import { Container, TransactionTypeContainer } from './styles'
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
 Modal.setAppElement('#root')
 
@@ -14,6 +15,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps){
+  const [type, setType] = useState('deposit')
+
   return(
       <Modal
         isOpen={isOpen}
@@ -42,19 +45,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           />
 
           <TransactionTypeContainer>
-            <button
+            <RadioBox
               type="button"
+              isActive={type === 'deposit'}
+              onClick={() => setType('deposit')}
             >
               <img src={incomeImg} alt="Entrada"/>
               <span>Entrada</span>
-            </button>
+            </RadioBox>
 
-            <button
+            <RadioBox
               type="button"
+              isActive={type === 'withdraw'}
+              onClick={() => setType('withdraw')}
             >
               <img src={outcomeImg} alt="Saída"/>
               <span>Saída</span>
-            </button>
+            </RadioBox>
 
           </TransactionTypeContainer>
 
